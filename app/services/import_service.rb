c7b103aab16b9af0064332cffaf002425ca2c5d8
Keolo Keagy <keolo@kea.gy>
2017-04-24 01:09:33 -0700

    Temporary error handling.

--- app/services/import_service.rb
+++ app/services/import_service.rb
@@ -8,6 +8,9 @@ class ImportService
       persist_event
       persist_playlist
       query_playlist
+    rescue StandardError => error
+        # TODO: Add comprehensive error handling
+        {}
       # rescue ImportService::Error => error
       #   # There was an error
       #   { errors: 'There was an error. Check the log for specifics.' }
